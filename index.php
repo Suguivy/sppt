d9396d167c7e7ef4c353a06c747b3af83be00f1a
Pòsweg <posweg@posweg.es>
2019-12-20 01:15:45 +0100

    Cleanup
    Cleaned things up, commented and set TODOs.

--- index.php
+++ index.php
@@ -30,27 +30,25 @@
 <body>
 <?php
 
-print "<a href=\"login.html\">Login (WIP)</a><br/><br/>\n\n";
+// TODO - Fix the login system to enable admin stuff
+//print "<a href=\"login.html\">Login (WIP)</a><br/><br/>\n\n";
 
-# Only create the directory if it doesn't exist
-/*if(!is_dir("db/".$date[year])){
-	mkdir("db/".$date[year],0744);
-	echo "Creada carpeta para el año " . $date[year] . "</br>\n";
-}*/
+// The date is used often, so I have set this variable to not call the
+// function everytime
+$now = getdate();
 
-/*
-$list = array(
-	array("1","2","3"), #Día
-	array("0","9","4") #Intensidad
-);
-*/
-
-if($_GET[y] and $_GET[m]){
+if(
+	// If date is passed in the URL, select that
+	$_GET[y] and $_GET[m] and 
+	// Prefer the else clause if the selected date is the same as
+	// the actual date
+	($_GET[y] != $now[year] or $_GET[m] != $now[mon]))
+{
 	$date = getdate(mktime(0, 0, 0, $_GET[m], 1, $_GET[y]));
 	$date[actual] = false;
 }	
 else{
-	$date = getdate();
+	$date = $now;
 	$date[actual] = true;
 }
 
@@ -61,23 +59,38 @@ foreach($list as $fields){
 	fputcsv($csv, $fields);
 }
 
-if(file_exists($dburl)) $csv = fopen($dburl, "w");
-else{
-	print "That month doesn't have a matrix to it.<br/>\n";
-}
+	/* Title */
 
-print "<h1>Productivity Matrix " . $date[month] . " " . $date[year] . "</h1>";
+print "<h1>Productivity Matrix " . $date[month] . " " . $date[year] . "</h1>\n";
+print "<br/>\n";
 
 
-fclose($csv);
+// Read the subjects file
 
+// TODO - This is gonna be inserted in the SQLite3 DB, but for
+// early development sake I'll let it be here
 $stats = file("stats", FILE_IGNORE_NEW_LINES);
 
-print "\t<form>\n";
+if(file_exists($dburl));
+else{
+	print "That month doesn't have a matrix to it.<br/>\n<br/>\n";
+}
+
+	/* Form */
+
+print "\t<form method=\"post\" action=\"submit.php\" >\n";
+
+// Hidden value to tell submit.php the month we're talking about
 print "\t\t<input type=\"hidden\" name=\"year\" value=\"".$date[year]."\">";
+
+	/* Table generation */
+
 print "\t<table>\n\t\t<tr>\n";
 print "\t\t\t<th></th>\n";
+
+// Header with the numbers
 for($i = 1; $i <= $days; $i++){
+	// Stay the day we're in if it is the actual month
 	if($i === $date[mday] and $date[actual]){
 		$today = " style=\"color:red;\" ";
 	}
@@ -87,16 +100,24 @@ for($i = 1; $i <= $days; $i++){
 	print "\t\t\t<th" . $today . ">" . $i . "</th>\n";
 }
 print "\t\t</tr>\n";
+
+// Rest of the table
 foreach ($stats as $line) {
+
+	// Print the subject name
 	print "\t\t<tr>\n\t\t\t<td>" . $line . "</td>\n";
+
+	// Print the cell for each day of the subject
 	for($i = 1; $i <= $days; $i++){
 		print "\t\t\t<td><input name=\"day[".$i."][".$line."]\" type=\"text\" maxlength=\"1\"></td>\n";
 	}
 	print "\t\t</tr>\n";
 }
-print "\t</table>\n";
+print "\t</table>\n<br/>\n";
+
 print "\t<p>File: " .  $dburl . "</p>";
 print "\n<br/>\n";
+
 print "<input type=\"submit\" class=\"button\">";
 print "</form>\n";
 ?>
---
+++ submit.php
@@ -0,0 +1,15 @@
+<?php
+
+// TODO
+//
+// The submit system isn't defined yet, but I leave this code
+// here that was before on the index.php file to implement it
+// later.
+
+# Only create the directory if it doesn't exist
+/*if(!is_dir("db/".$date[year])){
+        mkdir("db/".$date[year],0744);
+        echo "Creada carpeta para el año " . $date[year] . "</br>\n";
+}*/
+
+?>
